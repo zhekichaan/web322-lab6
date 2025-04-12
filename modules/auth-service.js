637f68eaf2c483a17eeeed412183ec26e973b6fd
Yevhen Chernytskyi <100925349+zhekichaan@users.noreply.github.com>
2025-04-11 22:09:58 -0400

    upd

--- modules/auth-service.js
+++ modules/auth-service.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
+const bcrypt = require("bcryptjs");
+
 require("dotenv").config();
 
 let userSchema = new Schema({
@@ -26,4 +28,86 @@ const initialize = () => {
       resolve();
     });
   });
+};
+
+const registerUser = (userData) => {
+  return new Promise(function (resolve, reject) {
+    if (userData.password == userData.password2) {
+      bcrypt
+        .hash(userData.password, 10)
+        .then((hash) => {
+          userData.password = hash;
+
+          let newUser = new User(userData);
+
+          newUser
+            .save()
+            .then(() => {
+              resolve(); // if everything is good -> resolve
+            })
+            .catch((err) => {
+              if (err.code == 11000) {
+                reject("User Name already taken"); // if error code is 11000 -> reject (duplicate key)
+              } else {
+                reject(`There was an error creating the user: ${err}`); // any other error -> reject
+              }
+            });
+        })
+        .catch(() => {
+          reject("There was an error encrypting the password");
+        });
+    } else {
+      reject("Passwords do not match");
+    }
+  });
+};
+
+const checkUser = (userData) => {
+  return new Promise(function (resolve, reject) {
+    User.find({ userName: userData.userName })
+      .exec()
+      .then((users) => {
+        if (users.length == 0) {
+          reject(`Unable to find user: ${userData.userName}`);
+        } else if (
+          !bcrypt
+            .compare(userData.password, users[0].password)
+            .then((result) => {
+              return result;
+            })
+        ) {
+          reject(`Incorrect Password for user: ${userData.userName}`);
+        } else {
+          if (users[0].loginHistory.length == 8) {
+            users[0].loginHistory.pop();
+          }
+
+          users[0].loginHistory.unshift({
+            dateTime: new Date().toString(),
+            userAgent: userData.userAgent,
+          });
+
+          User.updateOne(
+            { userName: users[0].userName },
+            { $set: { loginHistory: users[0].loginHistory } }
+          )
+            .exec()
+            .then(() => {
+              resolve(users[0]);
+            })
+            .catch((err) => {
+              reject(`There was an error verifying the user: ${err}`);
+            });
+        }
+      })
+      .catch((err) => {
+        reject(`Unable to find user: ${userData.userName}`);
+      });
+  });
+};
+
+module.exports = {
+  initialize,
+  registerUser,
+  checkUser,
 };
--- server.js
+++ server.js
@@ -13,12 +13,12 @@
 const projectData = require("./modules/projects");
 const authData = require("./modules/auth-service"); // import user auth module
 
-projectData.initialize();
+const clientSessions = require("client-sessions");
 
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require("path");
+// const path = require("path");
 const bodyParser = require("body-parser");
 
 app.set("views", __dirname + "/views");
@@ -27,86 +27,31 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(
+  clientSessions({
+    cookieName: "session", // this is the object name that will be added to 'req'
+    secret: "o6LjQ5EVNC28ZgK64hDELM18ScpFQr", // this should be a long un-guessable string.
+    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+const ensureLogin = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => res.render("home"));
 
 app.get("/about", (req, res) => res.render("about"));
-
-app.get("/solutions/addProject", (req, res) => {
-  projectData
-    .getAllSectors()
-    .then((sectorData) => {
-      res.render("addProject", { sectors: sectorData });
-    })
-    .catch((err) => {
-      res.status(404).render("404", {
-        message: err,
-      });
-    });
-});
-
-app.post("/solutions/addProject", (req, res) => {
-  projectData
-    .addProject(req.body)
-    .then(() => {
-      res.redirect("/solutions/projects");
-    })
-    .catch((err) => {
-      res.render("500", {
-        message: err,
-      });
-    });
-});
-
-app.get("/solutions/editProject/:id", (req, res) => {
-  projectData
-    .getAllSectors()
-    .then((sectorData) => {
-      projectData
-        .getProjectById(req.params.id)
-        .then((projectData) => {
-          res.render("editProject", {
-            sectors: sectorData,
-            project: projectData,
-          });
-        })
-        .catch((err) => {
-          res.status(404).render("404", {
-            message: err,
-          });
-        });
-    })
-    .catch((err) => {
-      res.status(404).render("404", {
-        message: err,
-      });
-    });
-});
-
-app.post("/solutions/editProject", (req, res) => {
-  projectData
-    .editProject(req.body)
-    .then(() => {
-      res.redirect("/solutions/projects");
-    })
-    .catch((err) => {
-      res.render("500", {
-        message: err,
-      });
-    });
-});
-
-app.get("/solutions/deleteProject/:id", (req, res) => {
-  projectData
-    .deleteProject(req.params.id)
-    .then(() => {
-      res.redirect("/solutions/projects");
-    })
-    .catch((err) => {
-      res.render("500", {
-        message: err,
-      });
-    });
-});
 
 app.get("/solutions/projects", (req, res) => {
   if (req.query.sector) {
@@ -140,6 +85,145 @@ app.get("/solutions/projects/:projectID", (req, res) => {
     });
 });
 
+app.get("/solutions/addProject", ensureLogin, (req, res) => {
+  projectData
+    .getAllSectors()
+    .then((sectorData) => {
+      res.render("addProject", { sectors: sectorData });
+    })
+    .catch((err) => {
+      res.status(404).render("404", {
+        message: err,
+      });
+    });
+});
+
+app.post("/solutions/addProject", ensureLogin, (req, res) => {
+  projectData
+    .addProject(req.body)
+    .then(() => {
+      res.redirect("/solutions/projects");
+    })
+    .catch((err) => {
+      res.render("500", {
+        message: err,
+      });
+    });
+});
+
+app.get("/solutions/editProject/:id", ensureLogin, (req, res) => {
+  projectData
+    .getAllSectors()
+    .then((sectorData) => {
+      projectData
+        .getProjectById(req.params.id)
+        .then((projectData) => {
+          res.render("editProject", {
+            sectors: sectorData,
+            project: projectData,
+          });
+        })
+        .catch((err) => {
+          res.status(404).render("404", {
+            message: err,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(404).render("404", {
+        message: err,
+      });
+    });
+});
+
+app.post("/solutions/editProject", ensureLogin, (req, res) => {
+  projectData
+    .editProject(req.body)
+    .then(() => {
+      res.redirect("/solutions/projects");
+    })
+    .catch((err) => {
+      res.render("500", {
+        message: err,
+      });
+    });
+});
+
+app.get("/solutions/deleteProject/:id", ensureLogin, (req, res) => {
+  projectData
+    .deleteProject(req.params.id)
+    .then(() => {
+      res.redirect("/solutions/projects");
+    })
+    .catch((err) => {
+      res.render("500", {
+        message: err,
+      });
+    });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    errorMessage: "",
+    userName: "",
+  });
+});
+
+app.post("/login", (req, res) => {
+  req.body.userAgent = req.get("User-Agent");
+
+  authData
+    .checkUser(req.body)
+    .then((user) => {
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory,
+      };
+      res.redirect("/solutions/projects");
+    })
+    .catch((err) => {
+      res.render("login", { errorMessage: err, userName: req.body.userName });
+    });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", {
+    errorMessage: "",
+    successMessage: "",
+    userName: "",
+  });
+});
+
+app.post("/register", (req, res) => {
+  authData
+    .registerUser(req.body)
+    .then(() => {
+      res.render("register", {
+        errorMessage: "",
+        successMessage: "User created",
+        userName: "",
+      });
+    })
+    .catch((err) => {
+      res.render("register", {
+        errorMessage: err,
+        successMessage: "",
+        userName: req.body.userName,
+      });
+    });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.reset();
+
+  res.render("home");
+});
+
+app.get("/userHistory", ensureLogin, (req, res) => {
+  res.render("userHistory");
+});
+
 app.use((req, res, next) => {
   res.status(404).render("404", {
     message: "I'm sorry, we're unable to find what you're looking for",
@@ -147,4 +231,14 @@ app.use((req, res, next) => {
 });
 
 // listen for connections
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+projectData
+  .initialize()
+  .then(authData.initialize)
+  .then(function () {
+    app.listen(port, function () {
+      console.log(`app listening on: ${port}`);
+    });
+  })
+  .catch(function (err) {
+    console.log(`unable to start server: ${err}`);
+  });
